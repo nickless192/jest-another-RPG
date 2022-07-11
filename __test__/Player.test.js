ee75cb4f530a25b21ad1559f8c8df97e9fecea7a
Omar Rodriguez <omar.rguez26@gmail.com>
2022-07-11 18:17:10 -0400

    updated Player class

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -17,4 +17,22 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     )
-})
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Omar');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test("gets inventory from player or returns false", () => {
+    const player = new Player('Omar');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
+});
